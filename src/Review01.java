557b8bfd49d1e095ad8691bb556ae6bca18d46c0
swallow-tail <nijiiroink@gmail.com>
2024-09-11 01:13:50 +0900

    消費税額の計算式を修正・戻り値はdouble型にしてからint型に変換

--- src/Review01.java
+++ src/Review01.java
@@ -2,16 +2,18 @@
 public class Review01 {
 
     public static void main(String[] args) {
-        //
         int price = 1500;
-        int taxrate = 10;
-        int result;
+        double taxrate = 0.1;
+        double result;
         result = taxMethod (price, taxrate);
-        System.out.println(price + "円の商品の税込価格は" + (price + result) + "円" + "（消費税は" + result + "円）です。");
+
+       //resultをdouble型からint型へ変換
+        System.out.println(price + "円の商品の税込価格は" + (price + (int)result) + "円" + "（消費税は" + (int)result + "円）です。");
     }
 
-        public static int taxMethod(int price, int taxrate) {
-        int num = price / taxrate;
+       //int型変数 price を一時的にdouble型に変換して除算する（結果はdouble型・戻り値あり)
+        public static double taxMethod(double price, double taxrate) {
+        double num = price * taxrate;
         return num;
     }
 
